546d9c8a3e4af4b82513eae4110dc32e9a3ef2ee
Claudio <c.merloni@gmail.com>
2022-05-04 23:18:06 +0200

    Create semgrep.lua

---
+++ lua/lspconfig/server_configurations/semgrep.lua
@@ -0,0 +1,22 @@
+local util = require 'lspconfig.util'
+
+local cmd = { 'python', '-m', 'semgrep_rpc.server', '--lsp', '--config', 'p/r2c' }
+
+if vim.fn.has 'win32' == 1 then
+  cmd = { 'cmd.exe', '/C', 'python', '-m', 'semgrep_rpc.server', '--lsp', '--config', 'p/r2c' }
+end
+
+return {
+  default_config = {
+    cmd = cmd,
+    filetypes = { 'python' },
+    root_dir = find_git_ancestor(fname) or vim.loop.os_homedir(),
+    single_file_support = true,
+  },
+  docs = {
+    description = [[
+https://github.com/returntocorp/semgrep
+A language server for Semgrep.
+]],
+  },
+}
